d912a2f88454f9cbe87264356004001c8ac9a591
Luis LC <luis.lazcanocruz@gmail.com>
2020-09-11 14:55:24 -0500

    Delete comments

--- Sort Algorithms/InsertionSort.ts
+++ Sort Algorithms/InsertionSort.ts
@@ -8,26 +8,25 @@ const array: number[] = [8,1,8,5,32,1,7,43,6,3,113,44,252,11];
 
 //Increasing Order
 function Increasing(arr: number[]){
+  let counter: number;
   for(let i = 1; i < arr.length; i++ ){
     let key= arr[i];
     let j= i-1;
     while(j>=0 && arr[j]>key){
       arr[j+1] = arr[j];
       j= j-1;
+      counter++;
     }
     arr[j+1]=key;
   }
-  return arr;
+  return counter;
 }
 
 // Decreasing Order
 function Decreasing(arr: Number[]){
   for(let i= 1; i<arr.length; i++ ){
-    // key = 8
     let key = arr[i];
-    // j = 0
     let j = i-1;
-    // 8 < 1
     while(j>=0 && arr[j]<key){
       arr[j+1]= arr[j];
       j=j-1;
