aacbd11fc68ff256b97699e4ad1fc3517d077786
Luis LC <luis.lazcanocruz@gmail.com>
2020-09-10 23:14:37 -0500

    Insertion Sort Increasing and Decreasing

--- Sort Algorithms/InsertionSort.ts
+++ Sort Algorithms/InsertionSort.ts
@@ -1,7 +1,13 @@
-console.time('Start')
+/**
+ * Introduction to algorithms
+ * Insertion Sort
+ */
+
+console.time('Start');
 const array: number[] = [8,1,8,5,32,1,7,43,6,3,113,44,252,11];
 
-function InsertionSort(arr: number[]){
+//Increasing Order
+function Increasing(arr: number[]){
   for(let i = 1; i < arr.length; i++ ){
     let key= arr[i];
     let j= i-1;
@@ -14,5 +20,23 @@ function InsertionSort(arr: number[]){
   return arr;
 }
 
-console.log(InsertionSort(array));
-console.timeEnd('Start')
+// Decreasing Order
+function Decreasing(arr: Number[]){
+  for(let i= 1; i<arr.length; i++ ){
+    // key = 8
+    let key = arr[i];
+    // j = 0
+    let j = i-1;
+    // 8 < 1
+    while(j>=0 && arr[j]<key){
+      arr[j+1]= arr[j];
+      j=j-1;
+    }
+    arr[j+1]=key
+  }
+  return arr;
+}
+
+console.log(Increasing(array));
+console.log(Decreasing(array));
+console.timeEnd('Start');
